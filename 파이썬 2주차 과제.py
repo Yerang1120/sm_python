645af26d876337c05630145a322a31a0e2c0329d
깜찍이 <150049595+Yerang1120@users.noreply.github.com>
2024-03-20 18:41:07 +0900

    Add files via upload

---
+++ 파이썬 2주차 과제.py
@@ -0,0 +1,8 @@
+def get_radius(prompt):
+    r=int(input(prompt))
+    return r
+def get_circle_area(radius):
+    ra=radius*radius*3.14
+    return ra
+a=get_radius("넓이를 구하고자 하는 원의 반지름은?")
+print("반지름이",a,"인 원의 넓이 = 3.14 x",a,"x",a,"=",(get_circle_area(a)))
