ca33f745843dea58120b5d6768314c16546c413c
깜찍이 <150049595+Yerang1120@users.noreply.github.com>
2024-04-24 20:36:39 +0900

    hw6.py

---
+++ hw6.py
@@ -0,0 +1,15 @@
+for k in range (1,10):
+    n=2
+    while n<5 :
+        print(f"{n} x {k} = {n*k:2d}",end="\t")
+        n+=1
+        if n == 5 :
+            print(f"{n} x {k} = {n*k:2d}",end="\n")
+print()
+for k in range (1,10):
+    n=6
+    while n<9 :
+        print(f"{n} x {k} = {n*k:2d}",end="\t")
+        n+=1
+        if n == 9 :
+            print(f"{n} x {k} = {n*k:2d}",end="\n")
